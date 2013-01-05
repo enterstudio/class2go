ca34a22800d2194c8ed817e7f63b63a54e1542e7
Huang Liang <exceedhl@gmail.com>
2013-01-05 20:51:05 +0800

    use default root user to execute so that it can access all dirs it needs

--- chef/cookbooks/class2go-collectstatic/recipes/default.rb
+++ chef/cookbooks/class2go-collectstatic/recipes/default.rb
@@ -1,7 +1,6 @@
 node["apps"].keys.each do |app|
     execute "collectstatic" do
         cwd node['system']['admin_home'] + "/#{app}/main"
-        user node['system']['admin_user']
         command "python manage.py collectstatic --noinput --clear"
     end
 end
